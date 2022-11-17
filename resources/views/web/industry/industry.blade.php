e19b69c99ba06c28931b48ba6a90b15c79cd8a43
Manoranjan Panigrahi <manoranjan.panigrahi9@gmail.com>
2022-11-17 17:37:55 +0530

    added hover box shadow in industry page

--- resources/views/web/industry/industry.blade.php
+++ resources/views/web/industry/industry.blade.php
@@ -1,25 +1,27 @@
-@extends("web.include.index")
+@extends('web.include.index')
 
-@section("content")
-<div class="breadcrums-container py-5" style="background-image: linear-gradient(45deg, rgb(0 0 0 / 65%), rgb(0 0 0 / 65%)),url(http://localhost/xcellentinsights/public/web/images/breadcrums/contact-us.jpg);">
-    <div class="container pt-5">
-        <div class="text-center mt-3">
-            <h3 class="breadcrumbs-title  fw-bold text-white">Industry Vertical</h3>
-        </div>
-    </div>
-</div>
-<div class="all-verticals-container mt-5">
-    <div class="container">
-        <div class="row">
-            @foreach($results as $result)
-            <div class="col-lg-3 mb-4">
-                <a href="#">
-                    <img src="{{asset('web/category/1650880205.webp')}}" alt="{{$result->name}}" style="height:210px; width: 270px;">
-                </a>
-                <i class="{{$result->icon}} industryIcon"></i>
+@section('content')
+    <div class="breadcrums-container py-5"
+        style="background-image: linear-gradient(45deg, rgb(0 0 0 / 65%), rgb(0 0 0 / 65%)),url(http://localhost/xcellentinsights/public/web/images/breadcrums/contact-us.jpg);">
+        <div class="container pt-5">
+            <div class="text-center mt-3">
+                <h3 class="breadcrumbs-title  fw-bold text-white">Industry Vertical</h3>
             </div>
-            @endforeach
         </div>
     </div>
-</div>
+    <div class="all-verticals-container mt-5">
+        <div class="container">
+            <div class="row justify-content-between">
+                @foreach ($results as $result)
+                    <div class="col-lg-3 mb-4 industry-category">
+                        <a href="#">
+                            <img src="{{ asset('web/category/1650880205.webp') }}" alt="{{ $result->name }}"
+                                style="height:210px; width: 270px;" class="img-fluid">
+                        </a>
+                        <i class="{{ $result->icon }} industryIcon"></i>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
