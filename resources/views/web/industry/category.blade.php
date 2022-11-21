af85e8c863cc99c084991e676b1b4d788f203dea
Manoranjan Panigrahi <manoranjan.panigrahi9@gmail.com>
2022-11-21 22:57:27 +0530

    accepted backend data for reports

--- resources/views/web/industry/category.blade.php
+++ resources/views/web/industry/category.blade.php
@@ -5,8 +5,8 @@
         style="background-image: linear-gradient(45deg, rgb(0 0 0 / 65%), rgb(0 0 0 / 65%)),url(http://localhost/xcellentinsights/public/web/images/breadcrums/contact-us.jpg);">
         <div class="container pt-5">
             <div class="text-center text-white mt-3">
-                <h3 class="breadcrumbs-title fw-bold ">{{$categories->name}}</h3>
-                <p class="mt-4 px-5">{{$categories->information}}</p>
+                <h3 class="breadcrumbs-title fw-bold ">{{ $categories->name }}</h3>
+                <p class="mt-4 px-5">{{ $categories->information }}</p>
             </div>
         </div>
     </div>
@@ -28,8 +28,10 @@
                         </div>
                         <ul class=" p-0">
                             @foreach ($results as $result)
-                                <li><a href="{{ route('category', ['category_slug' => $result['slug']]) }}">{{ $result->name }}</a></li>
-                            @endforeach 
+                                <li><a
+                                        href="{{ route('category', ['category_slug' => $result['slug']]) }}">{{ $result->name }}</a>
+                                </li>
+                            @endforeach
 
                         </ul>
                     </div>
@@ -38,6 +40,28 @@
                     </div>
                 </div>
                 <div class="col-lg-8">
+                    @foreach ($reports as $report)
+                        <div class="individual-report-container p-3">
+                            <h5>{{ $report->title }}</h5>
+                            <p>{!! substr($report->meta_desc, 0, 180) !!}</p>
+                            <div class="report-detail-bar py-2 px-3">
+                                <div class="row">
+                                    <div class="col-lg-4 ">
+                                        <h6 class="m-0">Report ID: {{ $report->unique_id }}</h6>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <h6 class="m-0">Publish date: {{ date('M Y', strtotime($result->publish)) }}</h6>
+                                    </div>
+                                    <div class="col-lg-2">
+                                        <h6 class="m-0">Pages: {{ $report->pages }}</h6>
+                                    </div>
+                                    <div class="col-lg-2">
+                                        <h6 class="m-0">$ {{ $report->single_price }}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
