f8254c840ee0bacfaa87461bfe2a1be0cfa904f1
rutvikaparwal <rutvika.parwal@candorworks.com>
2023-01-09 15:29:26 +0530

    fontawesome and flaticon files deleted

--- resources/views/web/include/style.blade.php
+++ resources/views/web/include/style.blade.php
@@ -20,7 +20,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
 
 {{-- FLATICON --}}
-<link rel="stylesheet" type="text/css" href="{{ url('/web/css/flaticon.css') }}">
+<!-- <link rel="stylesheet" type="text/css" href="{{ url('/web/css/flaticon.css') }}"> -->
 
 {{-- BOOTSTRAP JS --}}
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
