31b0062a22b65675c901472beb37397eddb525e0
rutvikaparwal <rutvika.parwal@candorworks.com>
2023-01-10 17:21:57 +0530

    background image changed

--- resources/views/web/report/enquiry.blade.php
+++ resources/views/web/report/enquiry.blade.php
@@ -1,7 +1,7 @@
 @extends('web.include.index')
 
 @section('content')
-<div class="breadcrums-container py-5" style="background-image: linear-gradient(45deg, rgb(0 0 0 / 65%), rgb(0 0 0 / 65%)),url(http://localhost/xcellentinsights/public/web/images/breadcrums/contact-us.jpg);">
+<div class="breadcrums-container py-5" style="background-image: linear-gradient(45deg, rgb(0 0 0 / 65%), rgb(0 0 0 / 65%)),url(http://localhost/xcellentinsights/public/web/images/breadcrums/contact-us.webp);">
     <div class="container pt-5">
         <div class="text-center mt-3 mb-5">
             <h2 class="breadcrumbs-title fw-bold text-white">{{ $heading }}</h2>
